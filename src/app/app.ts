029ac5beabf8965dcb149bc39d38eeefc4c27f2d
Maximilian Tertiuc <tertiuc@goldbeck.biz>
2026-06-11 15:47:30 +0200

    Refactor app structure by removing unused components and updating routing configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AboutMe } from './about-me/about-me';
-import { Career } from './career/career';
-import { Hero } from './hero/hero';
-import { Projects } from './projects/projects';
-import { Contact } from './contact/contact';
-import { Navbar } from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, AboutMe, Career, Projects, Contact, Navbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected readonly title = signal('onepager-persona');
-}
+export class App {}
